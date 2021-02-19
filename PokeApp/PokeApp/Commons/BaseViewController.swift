bdcbd86da8ee76300bc925ade2a7414b65ec082a
bedranfleck <affbedran@gmail.com>
2021-02-19 20:55:51 -0300

    Adding Coordinator protocol definition and Abstract class for ViewControllers

---
+++ PokeApp/PokeApp/Commons/BaseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BaseViewController.swift
+//  PokeApp
+//
+//  Created by André Felipe Fleck Bedran on 19/02/21.
+//
+
+import UIKit
+
+class BaseViewController<T>: UIViewController {
+    private(set) var viewModel: T
+    
+    init(viewModel: T) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ PokeApp/PokeApp/Commons/Coordinator.swift
@@ -0,0 +1,12 @@
+//
+//  Coordinator.swift
+//  PokeApp
+//
+//  Created by André Felipe Fleck Bedran on 19/02/21.
+//
+
+import Foundation
+
+protocol Coordinator {
+    func start()
+}
