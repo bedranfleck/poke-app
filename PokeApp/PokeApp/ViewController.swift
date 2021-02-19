611d504a7ee041f79765d904bc3d2bdc2056b53d
bedranfleck <affbedran@gmail.com>
2021-02-18 22:04:34 -0300

    Created base swift project.

---
+++ PokeApp/PokeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeApp
+//
+//  Created by André Felipe Fleck Bedran on 18/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
